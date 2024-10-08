e92ec0555c2a2798f0b74bb4ae507d89e3a51847
sanakhann99 <sanakhanofficially@gmail.com>
2024-10-08 14:31:32 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,32 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function romanToInt(s) {
+    // Define a mapping of Roman numerals to their integer values
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
 
+    let total = 0;
 
-module.exports={romanToInt}
+    // Loop through each character in the input string
+    for (let i = 0; i < s.length; i++) {
+        // Get the value of the current Roman numeral
+        const currentVal = romanMap[s[i]];
+        // Get the value of the next Roman numeral (if there is one)
+        const nextVal = romanMap[s[i + 1]];
+
+        // If the current value is less than the next value, subtract it
+        if (nextVal && currentVal < nextVal) {
+            total -= currentVal;
+        } else {
+            // Otherwise, add the current value to the total
+            total += currentVal;
+        }
+    }
+
+    return total;
+}
