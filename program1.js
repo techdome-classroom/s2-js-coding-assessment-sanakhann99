2dcd9c31f7678ec5138c711428235b5e4a8b2b14
sanakhann99 <sanakhanofficially@gmail.com>
2024-10-08 13:54:36 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -1,11 +1,26 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-var isValid = function(s) {
+function isValid(s) {
+    const stack = [];
+    const map = {
+        '(': ')',
+        '{': '}',
+        '[': ']'
+    };
+
+    for (let i = 0; i < s.length; i++) {
+        const char = s[i];
+
+        if (map[char]) {
+            stack.push(char);
+        } else {
+            const topElement = stack.pop();
+            if (map[topElement] !== char) {
+                return false;
+            }
+        }
+    }
     
-};
+    return stack.length === 0;
+}
 
 module.exports = { isValid };
-
 
